27ca08144861cb7db18c70b0a0bcb938c4b770c5
udonna-cell <orabuezestanley@gmail.com>
2024-04-04 09:06:00 +0100

    moving development

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const homeRouter = require("./routes/home")
+const detailRouter = require("./routes/detail")
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/home', homeRouter);
+app.use('/detail', detailRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
