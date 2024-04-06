302f09453cf94d39ffe8a6c8d0e73c445e45ef23
udonna-cell <orabuezestanley@gmail.com>
2024-04-06 14:20:49 +0100

    still working

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const homeRouter = require("./routes/home")
 const detailRouter = require("./routes/detail")
+const watchRouter = require("./routes/watch")
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/home', homeRouter);
 app.use('/detail', detailRouter);
+app.use('/watch', watchRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+const path = require("path")
+const fs = require("fs");
+const { log } = require('debug/src/browser');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  let movieFilePath = path.resolve(__dirname,"../public/movies/A Tourist's Guide to Love (2023) (NetNaija.com).mkv")
+  const stat = fs.statSync(movieFilePath);
+  const fileSize = stat.size;
+  const range = req.headers.range
+
+  if (range) {
+    const parts = range.replace(/bytes=/,"").split("-");
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+    const chunksize = (end - start) + 1;
+    const file = fs.createReadStream(movieFilePath, {start, end});
+
+    const head = {
+      "Content-Ranges": `bytes ${start}-${end}/${fileSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunksize,
+      "Content-Type": "video/mp4"
+    }
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      "Content-Length": fileSize,
+      "Content-Type": "video/mp4"
+    };
+    res.writeHead(200, head);
+    fs.createReadStream(movieFilePath).pipe(res)
+  }
 });
 
 module.exports = router;
